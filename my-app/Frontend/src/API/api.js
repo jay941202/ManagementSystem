86a4173db1e601e204cc0e7600610e50a5b7bfcb
jay941202 <zxcv4975@gmail.com>
2025-09-04 00:11:07 -0400

    API URL changed

--- my-app/Frontend/src/API/api.js
+++ my-app/Frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://managementsystem-6vse.onrender.com/api",
 });
 
 API.interceptors.request.use(
